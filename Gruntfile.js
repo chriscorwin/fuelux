edd6a6b3a5ae0ce097315e01b8a5232b6fd024a0
Chris Corwin <CCorwin@exacttarget.com>
2015-03-04 12:43:03 -0500

    Fix focus outline for checkbox, radio, especially with tabbing. Lax errors for section in HTML validation, since it is a recommendation, not a requirement, and in our case, having a heading in section is stupid.

--- Gruntfile.js
+++ Gruntfile.js
@@ -455,6 +455,7 @@ module.exports = function (grunt) {
 				},
 				stoponerror: true,
 				relaxerror: [//ignores these errors
+					'Section lacks heading. Consider using h2-h6 elements to add identifying headings to all sections.',
 					'Bad value X-UA-Compatible for attribute http-equiv on element meta.',
 					'Element head is missing a required instance of child element title.'
 				],
